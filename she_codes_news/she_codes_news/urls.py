195bf75abba03379f0bd391d7451f3b7c17d3894
irah rosete <irah.rosete@outlook.com>
2022-12-05 20:15:12 +1100

    create user login form and update urls

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('news/', include('news.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
