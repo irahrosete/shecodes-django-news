cb052b9ef2ff188ee58b945cbf00d568656e5717
irah rosete <irah.rosete@outlook.com>
2022-12-10 17:01:01 +1100

    update to render user profile

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateAccountView, UserProfileView
+from .views import CreateAccountView, UserStoryView, UserProfileView
 
 
 app_name = "users"
@@ -7,5 +7,6 @@ app_name = "users"
 urlpatterns = [
     path("create-account/", CreateAccountView.as_view(), name="createAccount"),
     # path("profile/<int:pk>", UserProfileView.as_view(), name="profile"),
+    path("user-story/<str:slug>", UserStoryView.as_view(), name="userStory"),
     path("profile/<str:slug>", UserProfileView.as_view(), name="profile"),
 ]
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -11,7 +11,14 @@ class CreateAccountView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "users/createAccount.html"
 
+class UserStoryView(generic.DetailView):
+    model = CustomUser
+    context_object_name = "userstory"
+    slug_field = "username"
+    template_name = "users/userStory.html"
+
 class UserProfileView(generic.DetailView):
     model = CustomUser
-    context_object_name = "user"
+    context_object_name = "profile"
     slug_field = "username"
+    template_name = "users/userProfile.html"
