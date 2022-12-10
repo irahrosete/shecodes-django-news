e0972ad0d9258ef700d5fe5e716acb8d3a045a2f
irah rosete <irah.rosete@outlook.com>
2022-12-10 14:39:27 +1100

    update users to present stories per user

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('news/', include('news.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    # path("users/", include('users.urls')),
+    path("users/", include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, UserProfileView
 
 
-pp_name = "users"
+app_name = "users"
 
 urlpatterns = [
     path("create-account/", CreateAccountView.as_view(), name="createAccount"),
+    # path("profile/<int:pk>", UserProfileView.as_view(), name="profile"),
+    path("profile/<str:slug>", UserProfileView.as_view(), name="profile"),
 ]
